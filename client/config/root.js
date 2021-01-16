08be28e26ef70baf1879269a4f7d8e52df1b57c4
sogekihei <seitensogekihei@protonmail.ch>
2021-01-16 18:56:07 +0300

    added history

--- client/config/root.js
+++ client/config/root.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+// import { ConnectedRouter } from 'connected-react-router'
 // import { Switch, Route } from 'react-router-dom'
 import store from '../redux'
-// import Home from '../components/home'
+import Home from '../components/home'
 
 const Root = () => {
   return (
     <Provider store={store}>
-      <div>
-        Hello!
-      </div>
+      <Home />
     </Provider>
   )
 }
+
+// const Root = (props) => {
+//   return (
+//     <Provider store={store}>
+//       <ConnectedRouter history={history} location={props.location} context={props.context}>
+//         <Switch>
+//           <Route exact path="/" component={() => <Home />} />
+//           <Route exact path="/dashboard" component={() => <Home />} />
+//         </Switch>
+//       </ConnectedRouter>
+//     </Provider>
+//   )
+// }
 
 export default Root
